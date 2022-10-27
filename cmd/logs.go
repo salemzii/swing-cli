4faa45a775d04284c345a74a89c08e3b3be2e4ba
salemzii <salemododa2@gmail.com>
2022-10-27 13:55:47 +0100

    bases cmd and logs cmd; package service

--- cmd/logs.go
+++ cmd/logs.go
@@ -1,1 +1,9 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+var logsCmd = cobra.Command{
+	Use:   "logs",
+	Short: "logs helps you access all operations on your logs",
+	Long:  `logs provides an array of operations that you can use to analyze your logs data`,
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,24 @@
 package cmd
 
-func Execute() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "swing",
+	Short: "distributed log as a service with ssdb",
+	Long:  `A Fast and Flexible cloud based log as a service software built on SingleStoreDb and jsonRPC`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
